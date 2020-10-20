7b43d81d141c77a5d6f8700487cf199349339f60
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-10-20 10:25:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,10 +40,10 @@
     <h3 style="color: red;">Go to my free website to enjoy my live stream</h3>
     <br>
    
-    <!-- <a id="imagen-rotativo" href="#"></a>
+    <a id="imagen-rotativo" href="#"></a>
     <a href="#"> <div class="iji" id="capa">
       <img src="https://lh3.googleusercontent.com/a8-C7XLtit92flEkLnKCJOvB9E7Ea0wOn5J21TGuSvIEGs2Zcfsts-UOOpz3gDP3jCaElnNlo1_dVNsAZQ=s1600"></div> </a>
-     -->
+    
 
 <!--  
 <a id="imagen-rotativo" href="#"></a>
