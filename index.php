479ca92f7e4ebd44fa92d383570d3affc80fb7b8
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-10-20 10:09:10 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,124 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:b="http://www.google.com/2005/gml/b" xmlns:data="http://www.google.com/2005/gml/data" xmlns:expr="http://www.google.com/2005/gml/expr"><head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  
+  
+  
+  
+  
+  
+  
+  
+  <!-- <meta http-equiv="refresh" content="300; url=https://Tracking-all-traffic.offerdirecto.com?id=LGoRTd6m85&type=21&click_id={click_id}&sub1={sub1}&sub2={sub2}&sub3={sub3}"> -->
+  
+  
+  
+  
+  
+  <style>
+    
+  
+  img{
+    width: 100px;
+    height: 100px;
+  
+  }
+  
+  .iji img{
+    width: 100%;
+    height: 200px;
+  }
+  
+  </style>
+  
+    </head>
+  
+  <body>
+    <!-- https://lh3.googleusercontent.com/a8-C7XLtit92flEkLnKCJOvB9E7Ea0wOn5J21TGuSvIEGs2Zcfsts-UOOpz3gDP3jCaElnNlo1_dVNsAZQ=s1600 -->
+  <center>
+    <br>
+    <h3 style="color: red;">Go to my free website to enjoy my live stream</h3>
+    <br>
+   
+    <!-- <a id="imagen-rotativo" href="#"></a>
+    <a href="#"> <div class="iji" id="capa">
+      <img src="https://lh3.googleusercontent.com/a8-C7XLtit92flEkLnKCJOvB9E7Ea0wOn5J21TGuSvIEGs2Zcfsts-UOOpz3gDP3jCaElnNlo1_dVNsAZQ=s1600"></div> </a>
+     -->
+
+<!--  
+<a id="imagen-rotativo" href="#"></a>
+<a href="#"> <div class="iji" id="capa">
+  <img src="https://i.ibb.co/xm7MJgy/Screenshot-233.png"></div> </a> -->
+
+
+
+ 
+
+<br>
+<br>
+ 
+
+<a id="imagen-rotativo" href="#"></a>
+  <a href="https://Tracking-all-traffic.offerdirecto.com?id=LGoRTd6m85&type=1&click_id={click_id}&sub1={sub1}&sub2={sub2}&sub3={sub3}&sub3={sub3}&source={source}"> <div class="iji" id="capa">
+    <img src="https://i.ibb.co/L0wJWsw/Screenshot-234.png"></div> </a>
+
+
+
+
+    <br>
+    <br>
+    
+
+
+
+ 
+
+  <a id="imagen-rotativo" href="#"></a>
+  <a href="https://redirect.adult.freelancercpa.net?id=528712938"> <div class="iji" id="capa">
+    <img src="https://i.ibb.co/b2fd2Xw/Screenshot-235.png"></div> </a>
+
+
+
+  <br>
+  <br>  
+
+
+  
+  </center>
+  
+  
+
+  
+  
+  
+  
+      <b:section id="main" showaddelement="yes">  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </b:section></body></html>
